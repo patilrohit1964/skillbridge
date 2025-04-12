b9efa3b0c3db2f0288d02f0f4edf8c7761727904
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-12 13:40:06 +0530

    routes added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
-
+const { z } = require("zod");
+const asyncHandler = require("express-async-handler");
+const argon2 = require("argon2");
 // api of register
 router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
