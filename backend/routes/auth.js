951c6a35b85f0643ec8e8706db1b4c201211f938
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-12 13:25:03 +0530

    routes added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("./models/User");
+const User = require("../models/user");
 
 // api of register
 router.post("/register", async (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,9 @@ app.use(morgan("dev"));
 app.get("/", (req, res) => {
   res.send("Api is running");
 });
-
+// routes
+const authRoutes = require("./routes/auth");
+app.use("/api/v1", authRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
