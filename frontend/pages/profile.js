1aea1a9e11173156f4b04ea7afbbc7d2654ec961
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-16 23:44:03 +0530

    input done

--- frontend/pages/profile.js
+++ frontend/pages/profile.js
@@ -8,22 +8,53 @@ const Profile = () => {
       <div className="bg-[#ddd] min-h-screen flex items-center justify-center">
         <div className="flex border border-red-500 max-w-7xl m-auto">
           <div className="max-w-3xl">rohit patil</div>
-          <div className="max-w-6xl">
+          <div className="max-w-6xl border border-blue-700 flex justify-between items-center">
             <div>
-              <div>
-                <label></label>
+              <div className="flex flex-col my-8">
+                <label>User Name</label>
+                <input
+                  type="text"
+                  name=""
+                  placeholder="Enter Your User Name"
+                //   value={""}
+                  className="border-b border-b-gray-500 outline-none"
+                />
               </div>
-              <div>
-                <label></label>
+              <div className="flex flex-col">
+                <label>E-MAIL</label>
+                <input
+                  type="text"
+                  name=""
+                  placeholder="Enter Your Email"
+                  value={""}
+                />
               </div>
-              <div>
-                <label></label>
+              <div className="flex flex-col my-8">
+                <label>PASSWORD</label>
+                <input
+                  type="text"
+                  name=""
+                  placeholder="Enter Your Password"
+                  value={""}
+                />
               </div>{" "}
-              <div>
-                <label></label>
+              <div className="flex flex-col">
+                <label>LOCATION</label>
+                <input
+                  type="text"
+                  name=""
+                  placeholder="Enter Your Location"
+                  value={""}
+                />
               </div>{" "}
-              <div>
-                <label></label>
+              <div className="flex flex-col my-8">
+                <label>PHONE</label>
+                <input
+                  type="text"
+                  name=""
+                  placeholder="Enter Your Phone"
+                  value={""}
+                />
               </div>
             </div>
             <div className="w-60 h-60 rounded-full overflow-hidden">
