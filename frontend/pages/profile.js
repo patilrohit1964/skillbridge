3dc0dd53b971703ceac3ce15f762f52ada3825cc
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-16 23:49:19 +0530

    profile ui inc

--- frontend/pages/profile.js
+++ frontend/pages/profile.js
@@ -6,54 +6,57 @@ const Profile = () => {
     <>
       <Navbar />
       <div className="bg-[#ddd] min-h-screen flex items-center justify-center">
-        <div className="flex border border-red-500 max-w-7xl m-auto">
-          <div className="max-w-3xl">rohit patil</div>
+        <div className="border border-red-500 max-w-7xl m-auto">
           <div className="max-w-6xl border border-blue-700 flex justify-between items-center">
             <div>
               <div className="flex flex-col my-8">
-                <label>User Name</label>
+                <label className="mb-4">User Name</label>
                 <input
                   type="text"
                   name=""
                   placeholder="Enter Your User Name"
-                //   value={""}
+                  //   value={""}
                   className="border-b border-b-gray-500 outline-none"
                 />
               </div>
               <div className="flex flex-col">
-                <label>E-MAIL</label>
+                <label className="mb-4">E-MAIL</label>
                 <input
                   type="text"
                   name=""
                   placeholder="Enter Your Email"
                   value={""}
+                  className="border-b border-b-gray-500 outline-none"
                 />
               </div>
               <div className="flex flex-col my-8">
-                <label>PASSWORD</label>
+                <label className="mb-4">PASSWORD</label>
                 <input
                   type="text"
                   name=""
                   placeholder="Enter Your Password"
                   value={""}
+                  className="border-b border-b-gray-500 outline-none"
                 />
               </div>{" "}
               <div className="flex flex-col">
-                <label>LOCATION</label>
+                <label className="mb-4">LOCATION</label>
                 <input
                   type="text"
                   name=""
                   placeholder="Enter Your Location"
                   value={""}
+                  className="border-b border-b-gray-500 outline-none"
                 />
               </div>{" "}
               <div className="flex flex-col my-8">
-                <label>PHONE</label>
+                <label className="mb-4">PHONE</label>
                 <input
                   type="text"
                   name=""
                   placeholder="Enter Your Phone"
                   value={""}
+                  className="border-b border-b-gray-500 outline-none"
                 />
               </div>
             </div>
@@ -62,6 +65,7 @@ const Profile = () => {
                 src="https://cdn.dribbble.com/userupload/41930657/file/still-92550ea561f7221c92447018141ae660.gif?resize=400x0"
                 className="h-full w-full"
               />
+
             </div>
           </div>
         </div>
